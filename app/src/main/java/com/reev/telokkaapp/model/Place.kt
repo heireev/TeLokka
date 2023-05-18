60b4742009bf22562796e687d654072a162732d3
heireev <muhammadrifqi216@gmail.com>
2023-05-18 14:04:38 +0800

    Initial commit Penambahan list mengikuti tutor (list pahlawan) dicoding

---
+++ app/src/main/java/com/reev/telokkaapp/model/Place.kt
@@ -0,0 +1,9 @@
+package com.reev.telokkaapp.model
+
+data class Place(
+    val id: String,
+    val name: String,
+    val category: String,
+    val photoUrl: String,
+    val mapUrl: String,
+)
---
+++ app/src/main/java/com/reev/telokkaapp/model/PlacesData.kt
@@ -0,0 +1,146 @@
+package com.reev.telokkaapp.model
+
+object PlacesData {
+    val places = listOf(
+        Place(
+            "1",
+            "Sarang Burung Waterfall",
+            "waterfall",
+            "https://www.google.com/maps/place/Sarang+Burung+Waterfall/data=!4m7!3m6!1s0x2d9589806f7037dd:0x48c59eadccfbe97d!8m2!3d-4.3634978!4d119.6953338!16s%2Fg%2F11bxfxhzvr!19sChIJ3Tdwb4CJlS0Rfen7zK2exUg?authuser=0&hl=en&rclk=1",
+            "https://www.google.com/maps/place/Sarang+Burung+Waterfall/data=!4m7!3m6!1s0x2d9589806f7037dd:0x48c59eadccfbe97d!8m2!3d-4.3634978!4d119.6953338!16s%2Fg%2F11bxfxhzvr!19sChIJ3Tdwb4CJlS0Rfen7zK2exUg?authuser=0&hl=en&rclk=1"
+        ),
+        Place(
+            "2",
+            "Waesai Waterfall",
+            "waterfall",
+            "https://lh5.googleusercontent.com/p/AF1QipO00uI-oij6R51N_YynjqrbyedVtfRZafKwRBSL=w138-h92-k-no",
+            "https://www.google.com/maps/place/Waesai+Waterfall/data=!4m7!3m6!1s0x2dbe3d26b9945b97:0xd240af00d9614b47!8m2!3d-4.5539866!4d119.7472339!16s%2Fg%2F11bzq3206h!19sChIJl1uUuSY9vi0RR0th2QCvQNI?authuser=0&hl=en&rclk=1"
+        ),
+        Place(
+            "3",
+            "Bukit Maddo",
+            "mountain",
+            "https://lh5.googleusercontent.com/p/AF1QipOVFkOSee6afKgCfu8nbRfIbSK03vg_XBDSqY-2=w138-h92-k-no",
+            "https://www.google.com/maps/place/Bukit+Maddo/data=!4m7!3m6!1s0x2dbe25d9b9f868b7:0xa93b40def23fe9eb!8m2!3d-4.4761733!4d119.6365193!16s%2Fg%2F11fx8v2t4l!19sChIJt2j4udklvi0R6-k_8t5AO6k?authuser=0&hl=en&rclk=1"
+        ),
+        Place(
+            "4",
+            "Bukit Lakeppo",
+            "mountain",
+            "https://lh5.googleusercontent.com/p/AF1QipME0r4ZruQgtOMG43x1TuXUskYc_xk0A0cIKooW=w138-h92-k-no",
+            "https://www.google.com/maps/place/Bukit+Lakeppo/data=!4m7!3m6!1s0x2dbe237f95ad4da5:0x5e84dc08dcc2f95a!8m2!3d-4.5076856!4d119.6979892!16s%2Fg%2F11fzfbxq7v!19sChIJpU2tlX8jvi0RWvnC3AjchF4?authuser=0&hl=en&rclk=1"
+        ),
+        Place(
+            "5",
+            "Taman Wisata Bola Pitue",
+            "park",
+            "https://lh5.googleusercontent.com/p/AF1QipO5o8b9N2_ZSXuOEDTpTkiin1bc9LIWpz9F6QXU=w122-h92-k-no",
+            "https://www.google.com/maps/place/Taman+Wisata+Bola+Pitue/data=!4m7!3m6!1s0x2d958807081a53bd:0x220f9cd1432fc4db!8m2!3d-4.3953002!4d119.6056649!16s%2Fg%2F11g8vnc35d!19sChIJvVMaCAeIlS0R28QvQ9GcDyI?authuser=0&hl=en&rclk=1"
+        ),
+        Place(
+            "6",
+            "Lappa Laona",
+            "mountain",
+            "https://lh5.googleusercontent.com/p/AF1QipMyjFKMycP53J6tSNikEFN6jWQ2NyykiqVTARIG=w80-h106-k-no",
+            "https://www.google.com/maps/place/Lappa+Laona/data=!4m7!3m6!1s0x2dbe3d796441554f:0xc82cc8690ec67bce!8m2!3d-4.5626829!4d119.7613837!16s%2Fg%2F11h6t3przn!19sChIJT1VBZHk9vi0RznvGDmnILMg?authuser=0&hl=en&rclk=1"
+        ),
+        Place(
+            "7",
+            "Dutungan Island",
+            "island",
+            "https://lh5.googleusercontent.com/p/AF1QipNaZgxCutT7eYUew3gDyHHv27OKOK2c40O5eiJV=w92-h92-k-no",
+            "https://www.google.com/maps/place/Dutungan+Island/data=!4m7!3m6!1s0x2d9597650804207f:0x49178197b4af4ced!8m2!3d-4.179977!4d119.620817!16s%2Fg%2F11j4nh61n_!19sChIJfyAECGWXlS0R7UyvtJeBF0k?authuser=0&hl=en&rclk=1"
+        ),
+        Place(
+            "8",
+            "Pantai Ujung Batu",
+            "beach",
+            "https://lh5.googleusercontent.com/p/AF1QipPeO6fEN70d8tfbOK5ZgqN0RS7lKDFIy07rBWJ7=w80-h142-k-no",
+            "https://www.google.com/maps/place/Pantai+Ujung+Batu/data=!4m7!3m6!1s0x2dbe2807383b4a79:0xb6e27f1921603cab!8m2!3d-4.4083153!4d119.6018042!16s%2Fg%2F11b6sx4nl4!19sChIJeUo7OAcovi0RqzxgIRl_4rY?authuser=0&hl=en&rclk=1"
+        ),
+        Place(
+            "9",
+            "To Magelli Waterfall",
+            "waterfall",
+            "https://lh5.googleusercontent.com/p/AF1QipNGFZ6vlUku6Pq-o0hu82D7mmFyafWuzB9_kzmc=w163-h92-k-no",
+            "https://www.google.com/maps/place/To+Magelli+Waterfall/data=!4m7!3m6!1s0x2d958be4d30d63b5:0x6fc6156a557b720!8m2!3d-4.3345057!4d119.7044969!16s%2Fg%2F11byyry6jx!19sChIJtWMN0-SLlS0RILdXpVZh_AY?authuser=0&hl=en&rclk=1"
+        ),
+        Place(
+            "10",
+            "Diana Waterpark",
+            "park",
+            "https://lh5.googleusercontent.com/p/AF1QipNsnHP_Gagmfv0Mhu2YI3F_UqdYNXfHkeFmhFV2=w204-h92-k-no",
+            "https://www.google.com/maps/place/Diana+Waterpark/data=!4m7!3m6!1s0x2d958ffe46dc5009:0xdca37867f92a2c6c!8m2!3d-4.2857215!4d119.6633444!16s%2Fg%2F11p59gt1gg!19sChIJCVDcRv6PlS0RbCwq-Wd4o9w?authuser=0&hl=en&rclk=1"
+        ),
+        Place(
+            "11",
+            "Pantai laguna",
+            "beach",
+            "https://lh5.googleusercontent.com/p/AF1QipM6jfoCIh3OUBFPR61Ka8PUp4U_QhEbgSlz8owZ=w122-h92-k-no",
+            "https://www.google.com/maps/place/Pantai+laguna/data=!4m7!3m6!1s0x2dbe2f5d6e458005:0x9d6c603aa318cee0!8m2!3d-4.49261!4d119.594005!16s%2Fg%2F11d_85dbbw!19sChIJBYBFbl0vvi0R4M4YozpgbJ0?authuser=0&hl=en&rclk=1"
+        ),
+        Place(
+            "12",
+            "Tanjung Asap Waterfall",
+            "waterfall",
+            "https://lh5.googleusercontent.com/p/AF1QipMFIsCKYVxV2ykFfi2GwmLEu83XBAhP1Uf_19lp=w122-h92-k-no",
+            "https://www.google.com/maps/place/Tanjung+Asap+Waterfall/data=!4m7!3m6!1s0x2dbe210cf8290dbd:0x3ac3a18917b108f6!8m2!3d-4.4486755!4d119.7346273!16s%2Fg%2F11gdkq71k2!19sChIJvQ0p-Awhvi0R9gixF4mhwzo?authuser=0&hl=en&rclk=1"
+        ),
+        Place(
+            "13",
+            "Manuba Waterfall",
+            "waterfall",
+            "https://lh5.googleusercontent.com/p/AF1QipPtbwx4hYQq7n4okcbjz28pXpcptFlVnAGBnQx3=w122-h92-k-no",
+            "https://www.google.com/maps/place/Manuba+Waterfall/data=!4m7!3m6!1s0x2d9591748f60bf3b:0x5e08ad83ee85dfa4!8m2!3d-4.2404314!4d119.6710347!16s%2Fg%2F11jhxqtcjt!19sChIJO79gj3SRlS0RpN-F7oOtCF4?authuser=0&hl=en&rclk=1"
+        ),
+        Place(
+            "14",
+            "Monumen Paccekke",
+            "park",
+            "https://lh5.googleusercontent.com/p/AF1QipMmP8pBtbtC4K5ROF9sRYjWZshM0Uht0xqrfsVr=w122-h92-k-no",
+            "https://www.google.com/maps/place/Monumen+Paccekke/data=!4m7!3m6!1s0x2d958dfced449917:0xb00128502906264e!8m2!3d-4.2605949!4d119.7005463!16s%2Fg%2F11c2rcy_l1!19sChIJF5lE7fyNlS0RTiYGKVAoAbA?authuser=0&hl=en&rclk=1"
+        ),
+        Place(
+            "15",
+            "Celebes Canyon",
+            "waterfall",
+            "https://lh5.googleusercontent.com/p/AF1QipN56BXMN7pMUtwWuqyzVwWeLF0KjB6FyOcJnLRX=w122-h92-k-no",
+            "https://www.google.com/maps/place/Celebes+Canyon/data=!4m7!3m6!1s0x2dbe249555555555:0x38d16abe5ce77903!8m2!3d-4.5008477!4d119.7163053!16s%2Fg%2F11bx2g7mb_!19sChIJVVVVVZUkvi0RA3nnXL5q0Tg?authuser=0&hl=en&rclk=1"
+        ),
+        Place(
+            "16",
+            "Wisata Alam baruttungnge",
+            "waterfall",
+            "https://lh5.googleusercontent.com/p/AF1QipOz1qohOQNLW4ARgvNOeyWz29d7IAX5WsufbJNB=w120-h92-k-no",
+            "https://www.google.com/maps/place/Wisata+Alam+baruttungnge/data=!4m7!3m6!1s0x2dbe25a293871841:0x836545dd017b601c!8m2!3d-4.5341361!4d119.6227683!16s%2Fg%2F11nntz0nk0!19sChIJQRiHk6Ilvi0RHGB7Ad1FZYM?authuser=0&hl=en&rclk=1"
+        ),
+        Place(
+            "17",
+            "Lasonrai Beach",
+            "beach",
+            "https://lh5.googleusercontent.com/p/AF1QipPNx_9J6xfMLoeGGMjio_IMZCMCejYPUSfmAASb=w122-h92-k-no",
+            "https://www.google.com/maps/place/Lasonrai+Beach/data=!4m7!3m6!1s0x2d9591e75ce15279:0xfbf8e9596edd166f!8m2!3d-4.2198025!4d119.609939!16s%2Fg%2F11j6sx2m42!19sChIJeVLhXOeRlS0Rbxbdblnp-Ps?authuser=0&hl=en&rclk=1"
+        ),
+        Place(
+            "18",
+            "Dermaga ujung indah Dutungan",
+            "beach",
+            "https://lh5.googleusercontent.com/p/AF1QipOGN9c2wy08fV8b0qthG8Kf8KHQiBmyORplT5Hz=w138-h92-k-no",
+            "https://www.google.com/maps/place/Dermaga+ujung+indah+Dutungan/data=!4m7!3m6!1s0x2d95977ce50fa47d:0x36bbcaa425f0a87c!8m2!3d-4.1813383!4d119.6246381!16s%2Fg%2F11p41dclmn!19sChIJfaQP5XyXlS0RfKjwJaTKuzY?authuser=0&hl=en&rclk=1"
+        ),
+        Place(
+            "19",
+            "Padang Indah Allepperengnge",
+            "mountain",
+            "https://lh5.googleusercontent.com/p/AF1QipOVvEtggp7rlpjYJ3p5ANmKv621P8peeS2wyJVt=w163-h92-k-no",
+            "https://www.google.com/maps/place/Padang+Indah+Allepperengnge/data=!4m7!3m6!1s0x2dbe3d174fb6d107:0xf57954c43b9bcc36!8m2!3d-4.5800377!4d119.741667!16s%2Fg%2F11h3dplckz!19sChIJB9G2Txc9vi0RNsybO8RUefU?authuser=0&hl=en&rclk=1"
+        ),
+        Place(
+            "20",
+            "Pulau Pannikiang",
+            "island",
+            "https://lh5.googleusercontent.com/p/AF1QipPysmdy5ZBQvHAITo-lfaGX2LYX42vCIpG24c88=w92-h92-k-no",
+            "https://www.google.com/maps/place/Pulau+Pannikiang/data=!4m7!3m6!1s0x2d958626580086a3:0x83c1650796d1089e!8m2!3d-4.3547403!4d119.5999139!16s%2Fg%2F11b63fpcqm!19sChIJo4YAWCaGlS0RngjRlgdlwYM?authuser=0&hl=en&rclk=1"
+        ),
+    )
+}
