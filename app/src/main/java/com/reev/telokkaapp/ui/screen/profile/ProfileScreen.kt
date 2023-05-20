645f97970f544bc56f4d5178f44d5efbbf69caf0
heireev <muhammadrifqi216@gmail.com>
2023-05-21 01:53:43 +0800

    Menambah ProfileScreen.kt untuk profil gweh

--- app/src/main/java/com/reev/telokkaapp/ui/screen/profile/ProfileScreen.kt
+++ app/src/main/java/com/reev/telokkaapp/ui/screen/profile/ProfileScreen.kt
@@ -1,13 +1,27 @@
 package com.reev.telokkaapp.ui.screen.profile
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.reev.telokkaapp.R
 import com.reev.telokkaapp.ui.theme.TeLokkaAppTheme
 
@@ -15,11 +29,43 @@ import com.reev.telokkaapp.ui.theme.TeLokkaAppTheme
 fun ProfileScreen(
     modifier: Modifier = Modifier
 ) {
-    Box(
+    val context = LocalContext.current
+    val url = "https://www.dicoding.com/users/heireev/academies"
+    Column(
         modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(stringResource(R.string.menu_profile))
+        Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = "Profile Picture",
+            modifier = Modifier
+                .size(120.dp)
+                .border(5.dp, MaterialTheme.colors.primary, shape = CircleShape)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "M. Rifqi Irawan",
+            style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp),
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "mrifqiirawan.unm@gmail.com",
+            style = TextStyle(fontSize = 16.sp),
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                context.startActivity(intent)
+            },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text(text = "Kunjungi Profil")
+        }
     }
 }
 
